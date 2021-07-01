c89cf63ddd93565c24578b1c0a7d66ca6a7e2f10
AstroCoding <hutchm6@mcmaster.ca>
2021-07-01 13:34:24 -0400

    Added project structure via Docker

---
+++ backend/api/health.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/jazzmoon/SawThat/backend/state"
+	"github.com/jazzmoon/SawThat/backend/util"
+)
+
+// health is the response format of "/health".
+type health struct {
+	Name         string `json:"name"`         // Name of application
+	Build        string `json:"build"`        // Build is the Git build hash
+	IsDocker     bool   `json:"isDocker"`     // IsDocker indicates if running in Docker
+	DatabasePing bool   `json:"databasePing"` // DatabasePing indicates if database is connected
+	Time         string `json:"time"`         // Time is the current server time
+	Uptime       string `json:"uptime"`       // Uptime is the uptime of the backend
+}
+
+// healthHandler is "/health". It returns the health of the backend.
+func healthHandler(s *state.State, w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	// ping database
+	databasePing := true
+	err := s.DB.Ping(ctx)
+	if err != nil {
+		databasePing = false
+	}
+
+	// generate response
+	now := time.Now().UTC()
+	util.Write(w, http.StatusOK, &health{
+		Name:         s.Config.Common.AppName,
+		Build:        s.Build,
+		IsDocker:     s.IsDocker,
+		DatabasePing: databasePing,
+		Time:         now.Format(time.RFC3339),
+		Uptime:       now.Sub(s.Start).String(),
+	})
+}
--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/AstroCoding/SawThatGoLang/backend/api"
 )
 
 func main() {
 	fmt.Println("Starting HTTP Server")
-	api.StartAPI()
 }
