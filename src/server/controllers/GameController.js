309324f5a657dd69dcea9e4ca5d2000423ea5096
Mark Hutchison <37601898+AstroCoding@users.noreply.github.com>
2022-12-10 05:16:24 -0500

    Update .gitignore to not include JS

--- src/server/controllers/GameController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.basicGet = void 0;
-async function basicGet() {
-    return {
-        hello: "world",
-    };
-}
-exports.basicGet = basicGet;
