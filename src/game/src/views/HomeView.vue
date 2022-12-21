5098d209fa1e1950a7ce17bad11958b00ac640ef
Vithralia <yaol5785@gmail.com>
2022-12-20 23:36:29 -0500

    Can copy game code to clipboard by clicking

--- src/game/src/views/HomeView.vue
+++ src/game/src/views/HomeView.vue
@@ -6,65 +6,72 @@ let serverURL = "URL";
 let gameCode = "123456";
 
 let players: Player[] = [
-{
-            name: "Player 1",
-            color: "#003FA3"
-        },
-        {
-            name: "Player 2",
-            color: "#00A324"
-        },
-        {
-            name: "Player 3",
-            color: "#A30000"
-        },
-        {
-            name: "Player 4",
-            color: "#A39C00"
-        },
-        {
-            name: "Player 5",
-            color: "#A39C00"
-        },        {
-            name: "Player 3",
-            color: "#A30000"
-        },
-        {
-            name: "Player 4",
-            color: "#A39C00"
-        },
-        {
-            name: "Player 5",
-            color: "#A39C00"
-        },        {
-            name: "Player 3",
-            color: "#A30000"
-        },
-        {
-            name: "Player 4",
-            color: "#A39C00"
-        },
-        {
-            name: "Player 5",
-            color: "#A39C00"
-        },        {
-            name: "Player 3",
-            color: "#A30000"
-        },
-        {
-            name: "Player 4",
-            color: "#A39C00"
-        },
-        {
-            name: "Player 5",
-            color: "#A39C00"
-        },
-        {
-            name: "Player 6",
-            color: "#CE5D5D"
-        }
+  {
+    name: "Player 1",
+    color: "#003FA3",
+  },
+  {
+    name: "Player 2",
+    color: "#00A324",
+  },
+  {
+    name: "Player 3",
+    color: "#A30000",
+  },
+  {
+    name: "Player 4",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 5",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 3",
+    color: "#A30000",
+  },
+  {
+    name: "Player 4",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 5",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 3",
+    color: "#A30000",
+  },
+  {
+    name: "Player 4",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 5",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 3",
+    color: "#A30000",
+  },
+  {
+    name: "Player 4",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 5",
+    color: "#A39C00",
+  },
+  {
+    name: "Player 6",
+    color: "#CE5D5D",
+  },
 ];
 
+function copyCode() {
+  navigator.clipboard.writeText(gameCode);
+  alert("Copied game code to clipboard.")
+}
 </script>
 
 <template>
@@ -73,14 +80,14 @@ let players: Player[] = [
       <div id="left">
         <LogoSVG id="icon" />
         <h1 id="title">SawThat?</h1>
-        <p>Game code:</p>
-        <p id="gamecode">{{gameCode}}</p>
-        <p>Go to {{serverURL}} and enter this code to join!</p>
+        <p>Game Code (Click to Copy):</p>
+        <button id="gamecode" @click="copyCode()">{{ gameCode }}</button>
+        <p>Go to {{ serverURL }} and enter this code to join!</p>
         <button @click="startGame()">Start Game</button>
       </div>
       <div id="right">
         <h2>Who's already here:</h2>
-        <PlayersListVue id="players" :players="players" :currentPlayer="null"/>
+        <PlayersListVue id="players" :players="players" :currentPlayer="null" />
       </div>
     </div>
   </main>
@@ -92,7 +99,7 @@ let players: Player[] = [
   flex-direction: row;
 }
 #title {
-  font-family: 'Fredericka the Great';
+  font-family: "Fredericka the Great";
   text-align: center;
   font-weight: 100;
   font-size: 5rem;
@@ -111,9 +118,9 @@ let players: Player[] = [
   width: fit-content;
   margin: auto;
   color: white;
-  background-color: #003FA3;
+  background-color: #003fa3;
   border-radius: 30px;
-  padding: 12px 24px;
+  /* padding: 12px 24px; */
 }
 #right {
   display: inline-block;
@@ -122,11 +129,12 @@ let players: Player[] = [
 }
 
 button {
-  border: 1rem solid #003FA3;
+  border: 1rem solid #003fa3;
   border-radius: 30px;
   margin: 8px;
   color: white;
   text-align: center;
-  background-color: #003FA3;
+  background-color: #003fa3;
+  min-width: 120px;
 }
 </style>
