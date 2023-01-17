4c2ea10768e72b021a31295f12dc6a0ff32222a9
Mark Hutchison <37601898+AstroCoding@users.noreply.github.com>
2023-01-16 21:56:02 -0500

    noId ins't a thing... Sad.

--- src/server/models/Game.ts
+++ src/server/models/Game.ts
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const playerPosition = new Schema(
-  {
-    player: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    position: { type: Number, required: true, default: 0 },
+const playerPosition = new Schema({
+  player: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  { noId: true }
-);
+  position: { type: Number, required: true, default: 0 },
+});
 
 const GameSchema = new Schema({
   hostId: {
