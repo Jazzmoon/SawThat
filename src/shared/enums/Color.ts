8daaea0634169c9021bf7e2654155215a2d1e941
Mark Hutchison <37601898+AstroCoding@users.noreply.github.com>
2023-01-21 10:55:06 -0500

    Add logic for adding users to the game player list

---
+++ src/shared/enums/Color.ts
@@ -0,0 +1,15 @@
+/**
+ * @file Color.ts
+ * @author Mark Hutchison
+ * An enum that details the exact options for player colors.
+ */
+export enum Color {
+  RED = "#FF0000",
+  ORANGE = "#FFA500",
+  YELLOW = "#FFFF00",
+  GREEN = "#008000",
+  BLUE = "#0000FF",
+  PURPLE = "#800080",
+  PINK = "#FFC0CB",
+  BROWN = "#A52A2A",
+}
