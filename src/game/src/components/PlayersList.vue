7eba50716f99224753269833bf769ad49595d451
Mark Hutchison <37601898+AstroCoding@users.noreply.github.com>
2023-01-21 14:33:07 -0500

    Fix colour to color

--- src/game/src/components/PlayersList.vue
+++ src/game/src/components/PlayersList.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="players">
         <div v-for="(player, index) in props.players" :key="player.username" :class="['player', (props.currentPlayer !== null && props.currentPlayer === index) ? 'chosen' : '']">
-            <div class="colorIndicator" :style="`border-color: ${player.colour}`"></div>
+            <div class="colorIndicator" :style="`border-color: ${player.color}`"></div>
             <p class="playerName">{{player.username}}</p>
         </div>
     </div>
