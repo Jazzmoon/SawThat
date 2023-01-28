172241cd0ed3a8c9dd09bb6168e78c47c5f6ef2d
Vithralia <yaol5785@gmail.com>
2023-01-28 17:29:05 -0500

    Display current player in client MainView

--- src/client/src/views/MainView.vue
+++ src/client/src/views/MainView.vue
@@ -1,18 +1,38 @@
 <template>
-    <main>
-        <div id="root">
-            <div>
-                <img id="logo" :src="LogoSVG"/>
-                <h2 id="title">Leaderboard</h2>
-            </div>
-            <div id="players">
-                <div v-for="(player, index) in props.players" :key="player.username" :class="['player', props.currentPlayerIndex === index ? 'chosen' : '']">
-                    <div class="colorIndicator" :style="`border-color: ${player.color}`"></div>
-                    <p class="playerName">{{`${index + 1}. ${player.username}`}}</p>
-                </div>
-            </div>
+  <main>
+    <div id="root">
+      <div>
+        <img id="logo" :src="LogoSVG" />
+        <h2 id="title">Leaderboard</h2>
+      </div>
+      <div>
+        <h4>
+          You are:
+          <div
+            class="colorIndicator"
+            :style="`border-color: ${props.players[currentPlayerIndex].color}`"
+          ></div>
+          {{ props.players[currentPlayerIndex].username }}
+        </h4>
+      </div>
+      <div id="players">
+        <div
+          v-for="(player, index) in props.players"
+          :key="player.username"
+          :class="[
+            'player',
+            props.currentPlayerIndex === index ? 'chosen' : '',
+          ]"
+        >
+          <div
+            class="colorIndicator"
+            :style="`border-color: ${player.color}`"
+          ></div>
+          <p class="playerName">{{ `${index + 1}. ${player.username}` }}</p>
         </div>
-    </main>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts" setup>
@@ -20,59 +40,59 @@ import LogoSVG from "@/assets/logo.svg";
 import type { Player } from "../../../shared/types/Player";
 
 const props = defineProps<{
-    players: Player[],
-    currentPlayerIndex: number
+  players: Player[];
+  currentPlayerIndex: number;
 }>();
-
 </script>
 
 <style scoped>
 #title {
-    font-family: 'Fredericka the Great';
-    font-weight: 100;
-    text-align: center;
+  font-family: "Fredericka the Great";
+  font-weight: 100;
+  text-align: center;
 }
 #root {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 150px 1fr;
+  margin-top: 48px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 150px 1fr;
 }
 #logo {
-    height: 75px;
-    width: auto;
+  height: 75px;
+  width: auto;
 }
 #players {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 
 .player {
-    height: 50px;
-    margin: 0 6px;
-    display: flex;
-    flex-direction: row;
-    padding: 6px;
+  height: 50px;
+  margin: 0 6px;
+  display: flex;
+  flex-direction: row;
+  padding: 6px;
 }
 
 .player.chosen {
-    border-radius: 15px;
-    border: 5px solid #003fa3;
+  border-radius: 15px;
+  border: 5px solid #003fa3;
 }
 
 .colorIndicator {
-    border: 10px solid #fff;
-    border-radius: 50%;
-    background-color: transparent;
-    height: 15px;
-    width: 15px;
-    margin-top: 7px;
+  border: 10px solid #fff;
+  border-radius: 50%;
+  background-color: transparent;
+  height: 15px;
+  width: 15px;
+  margin-top: 7px;
 }
 
 .playerName {
-    color: white;
-    line-height: 50px;
-    vertical-align: middle;
-    margin: 0;
-    margin-left: 12px;
+  color: white;
+  line-height: 50px;
+  vertical-align: middle;
+  margin: 0;
+  margin-left: 12px;
 }
 </style>
