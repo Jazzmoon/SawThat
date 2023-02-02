e930be9efcb99286c0c9cbfb1af8f83f87603535
Daniel Genkin <genkind@gmail.com>
2023-02-02 15:53:38 -0500

    planned out tests

--- tst/shared/Base_WS_API_spec.ts
+++ tst/shared/Base_WS_API_spec.ts
@@ -1,4 +1,36 @@
 
-test('test', () => {
+test('Test Base_WS_API is created properly when setting up a connection', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API fails properly when it fails to establish a connection', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API message callback setters and getters work', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API reset connection destroys everything properly', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API handles new messages properly', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API handles empty messages properly', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API handles unformatted messages properly', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API sendRequest make requests properly', () => {
+    expect(1+2).toBe(3);
+});
+
+test('Test Base_WS_API sendRequest fails properly when connection is not established', () => {
     expect(1+2).toBe(3);
 });
