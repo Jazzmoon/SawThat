d279bbfdc19b2d0ded6a65098a13a90fdd2781a5
Mark Hutchison <37601898+AstroCoding@users.noreply.github.com>
2023-02-12 13:29:57 -0500

    Finished mongoose test cases for models.

--- src/server/tests/models.test.ts
+++ src/server/tests/models.test.ts
@@ -4,25 +4,23 @@
  * Tests the Game Logic and Mongoose interactions.
  */
 
-import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-import Game, { GameType } from "../models/Game";
+import Game from "../models/Game";
 import User, { UserType } from "../models/User";
 
-dotenv.config();
+const DATABASE_URL = `mongodb://localhost:27017`,
+  DATABASE_USER = `sawthat`,
+  DATABASE_PASS = `sawthatsecretpass`;
 
 // Constants and Variables
 const theme_pack = "disney";
 
 // Set-Up and Teardown
 beforeAll(async () => {
-  mongoose.connect(`${process.env.DATABASE_URL!}`, {
-    user: process.env.DATABASE_USER!,
-    pass: process.env.DATABASE_PASS!,
+  await mongoose.connect(DATABASE_URL, {
+    user: DATABASE_USER,
+    pass: DATABASE_PASS,
   });
-  const db = mongoose.connection;
-  db.on("error", console.error);
-  db.once("open", () => console.log("Database connection established."));
   const newGame = new Game({
     hostId: null,
     game_code: "0000",
@@ -66,34 +64,46 @@ beforeAll(async () => {
 
 afterAll(async () => {
   // Retrieve the game for the game information
-  const game = await Game.findOne({
+  await Game.findOneAndDelete({
     game_code: "0000",
-  })
-    .orFail()
-    .exec();
+  }).exec();
   // Delete users
-  for (let userId in game.players) await User.findByIdAndDelete(userId).exec();
-  await User.findByIdAndDelete(game.hostId).exec();
-
-  // Delete game
-  await game.delete();
+  await User.findOneAndDelete({
+    username: "game0000",
+    token: "Game-game0000-0000",
+  }).exec();
+  await User.findOneAndDelete({
+    username: "user0000_1",
+    token: "Client-user0000_1-0000",
+  }).exec();
+  await User.findOneAndDelete({
+    username: "user0000_2",
+    token: "Client-user0000_2-0000",
+  }).exec();
+  // Disconnect mongoose
+  await mongoose.disconnect();
 });
 
 // Test Population
-describe("Population and De-Population", async () => {
-  const game = await Game.findOne({ game_code: "0000" })
-    .populate<UserType>("hostId")
-    .populate<UserType[]>("players")
-    .orFail()
-    .exec();
+describe("Population and De-Population", () => {
   test("Population", async () => {
+    const game = await Game.findOne({ game_code: "0000" })
+      .populate<UserType>("hostId")
+      .populate<UserType[]>("players")
+      .orFail()
+      .exec();
     expect(game.populated("hostId")).toBeTruthy();
     expect(game.hostId).not.toBeInstanceOf(mongoose.Types.ObjectId);
     expect(game.populated("players")).toBeTruthy();
     expect(game.players[0]).not.toBeInstanceOf(mongoose.Types.ObjectId);
   });
-  game.depopulate("hostId").depopulate("players");
   test("De-Population", async () => {
+    const game = await Game.findOne({ game_code: "0000" })
+      .populate<UserType>("hostId")
+      .populate<UserType[]>("players")
+      .orFail()
+      .exec();
+    game.depopulate("hostId").depopulate("players");
     expect(game.populated("hostId")).toBeFalsy();
     expect(game.hostId).toBeInstanceOf(mongoose.Types.ObjectId);
     expect(game.populated("players")).toBeFalsy();
@@ -101,10 +111,87 @@ describe("Population and De-Population", async () => {
   });
 });
 
-// Test De-Population
-
 // Test Changing Turn Order
+describe("Turn Order", () => {
+  test("Game Order", async () => {
+    const game = await Game.findOne({ game_code: "0000" })
+        .populate<UserType>("hostId")
+        .populate<UserType[]>("players")
+        .orFail()
+        .exec(),
+      user1 = await User.findOne({
+        username: "user0000_1",
+        token: "Client-user0000_1-0000",
+      })
+        .orFail()
+        .exec(),
+      user2 = await User.findOne({
+        username: "user0000_2",
+        token: "Client-user0000_2-0000",
+      })
+        .orFail()
+        .exec();
+    expect(game.players.map((p) => p._id)).toEqual([user1._id, user2._id]);
+    game.players = game.players.concat(game.players.shift()!);
+    await game.save();
+    expect(game.players.map((p) => p._id)).toEqual([user2._id, user1._id]);
+  });
+  test("Game Order Remains Saved", async () => {
+    const game = await Game.findOne({ game_code: "0000" })
+        .populate<UserType>("hostId")
+        .populate<UserType[]>("players")
+        .orFail()
+        .exec(),
+      user1 = await User.findOne({
+        username: "user0000_1",
+        token: "Client-user0000_1-0000",
+      })
+        .orFail()
+        .exec(),
+      user2 = await User.findOne({
+        username: "user0000_2",
+        token: "Client-user0000_2-0000",
+      })
+        .orFail()
+        .exec();
+    expect(game.players.map((p) => p._id)).toEqual([user2._id, user1._id]);
+  });
+});
 
 // Test Deleting a User
+describe("Deleting a User", () => {
+  test("Check if User is Deleted", async () => {
+    await User.findOneAndDelete({
+      username: "user0000_1",
+      token: "Client-user0000_1-0000",
+    })
+      .orFail()
+      .exec();
+    const user1 = await User.findOne({
+        username: "user0000_1",
+        token: "Client-user0000_1-0000",
+      }).exec(),
+      user2 = await User.findOne({
+        username: "user0000_2",
+        token: "Client-user0000_2-0000",
+      }).exec();
+    expect(user1).toBeFalsy();
+    expect(user1).toBeNull();
+    expect(user2).toBeTruthy();
+    expect(user2).not.toBeNull();
+  });
+});
 
 // Test Deleting the Game
+describe("Deleting a Game", () => {
+  test("Check if Game is Deleted", async () => {
+    await Game.findOneAndDelete({
+      game_code: "0000",
+    }).exec();
+    const game = await Game.findOne({
+      game_code: "0000",
+    }).exec();
+    expect(game).toBeFalsy();
+    expect(game).toBeNull();
+  });
+});
